4216cf869cd3dad086fa54650f47ce1cba265ed1
Eric J <ericj31415@gmail.com>
2024-12-09 14:42:13 -0600

    add data reading

--- src/runner.py
+++ src/runner.py
@@ -1,9 +1,24 @@
 # Main script
+import glob
+import pandas as pd
 
 from storage_system import System
 
 # Read data
+main_dataframe = pd.DataFrame()
+data_files = glob.glob("data/names/*.txt")
+data_list = []
+for file in data_files:
+  sub_data = pd.read_csv(file, sep=',', names=["Name", "Sex", "Frequency"])
+  data_list.append(sub_data)
+main_dataframe = pd.concat(data_list, axis=0)
+names = main_dataframe.iloc[:, 0]
+names = names.unique()
+print(f"There are {len(names)} unique names.")
 
+main_dataframe = pd.read_csv("data/medium_articles.csv")
+articles = main_dataframe.iloc[:, 0]
+print(f"There are {len(articles)} articles.")
 
 # Set up baseline system
 usernames = set([])
